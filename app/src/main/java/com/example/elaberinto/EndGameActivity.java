f6848d03a25a042fd07563412f1a10c195f15b93
Alexis Sánchez Rodríguez <ayzr@outlook.com>
2019-07-17 02:08:14 -0400

    Added restart button to end activity

--- app/src/main/java/com/example/elaberinto/EndGameActivity.java
+++ app/src/main/java/com/example/elaberinto/EndGameActivity.java
@@ -1,6 +1,9 @@
 package com.example.elaberinto;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,13 @@ public class EndGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_game);
+        Button restart = findViewById(R.id.restart_button);
+        restart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(EndGameActivity.this, StartGameActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
